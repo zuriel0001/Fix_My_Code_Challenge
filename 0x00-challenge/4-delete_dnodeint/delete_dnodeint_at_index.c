1fc5a7d738e3d356c027c4484f085df45cd86a00
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-24 01:47:10 +0000

    correcyed script

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,11 +43,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = *head;
-		(*head)->prev->next = (*head)->next;
+		tmp = *head; /* initialized tmp */
+		(*head)->prev->next = (*head)->next; /* previewsly (*head)->prev->prev = (*head)->prev */
 		free(*head);
 		if ((*head)->next)
-			(*head)->next->prev = tmp->prev;
+			(*head)->next->prev = tmp->prev; /* previewsly (*head)->next->prev = (*head)->prev */
 		*head = saved_head;
 	}
 	return (1);
